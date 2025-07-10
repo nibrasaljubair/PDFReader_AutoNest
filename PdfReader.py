b1bc417872211842e6f0496d3b2d3e60a3d49b0e
Nibras Aljubair <nibrasaljubair@Nibrass-MacBook-Air.local>
2025-07-09 21:44:53 -0400

    Initial commit with script reading pdf and printing

---
+++ PdfReader.py
@@ -0,0 +1,13 @@
+import pypdf 
+import pandas as pd
+
+with open('INV-1000.pdf', 'rb') as file:
+    reader = pypdf.PdfReader(file)
+    num_pages = len(reader.pages)
+    print(f"Number of pages: {num_pages}")
+    
+    for page_num in range(num_pages):
+        page = reader.pages[page_num]
+        text = page.extract_text()
+        print(f"\n--- Page {page_num + 1} ---")
+        print(text)
