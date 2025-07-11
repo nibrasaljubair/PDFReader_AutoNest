80fa7e75c96fa23848f2daca41601340f737c873
Nibras Aljubair <nibrasaljubair@Nibrass-MacBook-Air.local>
2025-07-10 23:49:14 -0400

    added string into list

--- PdfReader.py
+++ PdfReader.py
@@ -1,13 +1,19 @@
 import pypdf 
 import pandas as pd
 
+pdftext = []
+
 with open('INV-1000.pdf', 'rb') as file:
     reader = pypdf.PdfReader(file)
     num_pages = len(reader.pages)
     print(f"Number of pages: {num_pages}")
-    
     for page_num in range(num_pages):
         page = reader.pages[page_num]
         text = page.extract_text()
+        print(len(text))
         print(f"\n--- Page {page_num + 1} ---")
         print(text)
+        pdftext.append(text)
+
+
+print(pdftext)
